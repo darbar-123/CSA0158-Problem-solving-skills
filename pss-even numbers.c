9c05018255262c61a7425e8aebf52320aaeaaa48
darbar-123 <112846580+darbar-123@users.noreply.github.com>
2023-02-01 22:10:55 +0530

    Add files via upload

---
+++ pss-even numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+  int i,num;
+  printf("\n  Enter the Maximum num Value : ");
+  scanf("%d", &num);
+  printf("Even numbers in the list are : ");
+  for(i = 1; i <= num; i++)
+  {
+  	if ( i%2 == 0 )
+  	{
+        printf("%d \t",i);
+  	}
+  }
+}
+
